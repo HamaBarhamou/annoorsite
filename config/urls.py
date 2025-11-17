8d340fa6903dcc0c37b0151716b2101a1b978660
HAMA Barhamou <hamabarhamou@gmail.com>
2025-11-17 15:13:46 +0100

    Fix: servir /media/ en local & prod via Django (route re_path + static_serve). Résout les 404 sur images blog/projets.

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.sitemaps.views import sitemap
 from sitecontent.sitemaps import (
     ServiceSitemap,
@@ -9,6 +9,7 @@ from sitecontent.sitemaps import (
     PostSitemap,
     StaticSitemap,
 )
+from django.views.static import serve as static_serve
 
 
 sitemaps = {
@@ -31,6 +32,15 @@ urlpatterns = [
     path("ckeditor/", include("ckeditor_uploader.urls")),
 ]
 
+
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        static_serve,
+        {"document_root": settings.MEDIA_ROOT, "show_indexes": False},
+        name="media",
+    ),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
